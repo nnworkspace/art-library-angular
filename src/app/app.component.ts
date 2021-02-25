121f6849a4f03494f1c1c78bd6ab681113436d6a
Ning Zhao <nnworkspace@gmail.com>
2021-02-25 09:27:32 +0100

    add cool faces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import faces from '../assets/coolFaces.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'art-library-angular';
+  public coolFaces: string[] = faces;
+  public coolFace = '';
+
+  ngOnInit(): void{
+    const random = Math.floor(Math.random() * this.coolFaces.length);
+    this.coolFace = this.coolFaces[random];
+    console.log(this.coolFace);
+  }
 }
