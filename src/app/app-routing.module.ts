3fc123e751290a222fbd3344820c3b7f96b00c55
Ning Zhao <nnworkspace@gmail.com>
2021-02-25 15:30:49 +0100

    create the app component structure, close #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import {LoginComponent} from './auth/login/login.component';
+import {InventoryComponent} from './inventory/inventory.component';
+import {LendingComponent} from './lending/lending.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'lending', component: LendingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { LendingComponent } from './lending/lending.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InventoryComponent,
+    LendingComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
