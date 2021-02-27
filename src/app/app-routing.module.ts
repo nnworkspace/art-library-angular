3d64d48270265f0bc29e98d9a9c422a0c0effe3a
Ning Zhao <nnworkspace@gmail.com>
2021-02-27 17:10:28 +0100

    add auth guard, #11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {LoginComponent} from './auth/login/login.component';
 import {InventoryComponent} from './inventory/inventory.component';
-import {BorrowingComponent} from "./borrowing/borrowing.component";
+import {BorrowingComponent} from './borrowing/borrowing.component';
+import {AuthGuard} from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'inventory', component: InventoryComponent },
-  { path: 'borrowing', component: BorrowingComponent }
+  { path: 'inventory', component: InventoryComponent, canActivate: [AuthGuard] },
+  { path: 'borrowing', component: BorrowingComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
